84cce22856b9cf108d9d8ada41c9ab51fb2277ea
owen.huang <firebushtree@163.com>
2023-02-10 23:28:23 +0800

    chore: update targetStack

--- src/observer/dep.ts
+++ src/observer/dep.ts
@@ -1,4 +1,24 @@
+import Watcher from './watcher'
+let id = 0
+
 export default class Dep {
+  static target: null | Watcher
+  id: number
+
   constructor() {
+    this.id = id++
   }
+}
+
+Dep.target = null
+const targetStack: Array<Watcher> = []
+
+export function pushTarget(target: Watcher) {
+  targetStack.push(target)
+  Dep.target = target
+}
+
+export function popTarget() {
+  targetStack.pop()
+  Dep.target = targetStack[targetStack.length - 1]
 }
