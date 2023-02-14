615f2372458b83defd3280bb2179436de5d9a68d
owen.huang <firebushtree@163.com>
2023-02-14 22:29:28 +0800

    chore: add gen attr

--- src/vdom/patch.ts
+++ src/vdom/patch.ts
@@ -1,26 +1,35 @@
-import VNode from "./vnode";
+import VNode from './vnode'
 
 export function patch(oldVNode: VNode | Element | undefined, newVNode: VNode) {
   createElm(newVNode)
 }
 
+export function genAttr(node: HTMLElement, data: any) {
+  if (data.attrs) {
+    Object.keys(data.attrs).forEach(key => {
+      node.setAttribute(key, data.attrs[key])
+    })
+  }
+}
+
 export function createElm(vnode: VNode, parentEl?: any) {
-    const data = vnode.data
-    const children = vnode.children
-    const tag = vnode.tag
-    if (tag) {
-      vnode.elm = document.createElement(tag as keyof HTMLElementTagNameMap)
-    } else if (vnode.text) {
-      vnode.elm = document.createTextNode(vnode.text)
-    }
+  const data = vnode.data
+  const children = vnode.children
+  const tag = vnode.tag
+  if (tag) {
+    vnode.elm = document.createElement(tag as keyof HTMLElementTagNameMap)
+    genAttr(vnode.elm, data)
+  } else if (vnode.text) {
+    vnode.elm = document.createTextNode(vnode.text)
+  }
 
-    if (parentEl) {
-      parentEl.appendChild(vnode.elm)
-    }
+  if (parentEl) {
+    parentEl.appendChild(vnode.elm)
+  }
 
-    if (Array.isArray(children)) {
-      children.forEach(item => {
-        createElm(item, vnode.elm)
-      })
-    }
+  if (Array.isArray(children)) {
+    children.forEach(item => {
+      createElm(item, vnode.elm)
+    })
+  }
 }
